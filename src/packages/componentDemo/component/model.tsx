2038953bf5d0b514f1c9170b90f2296e92fa0354
刘少杰 <liushaojie@cvte.com>
2021-06-07 15:10:11 +0800

    feat: tojsonx不应开发者自行定义

---
+++ src/packages/componentDemo/component/model.tsx
@@ -0,0 +1,95 @@
+export default function(dependencies) {
+  const { ElementModel } = dependencies.kernel;
+  return class ComponentModel extends ElementModel {
+    // 是否开始debug输出
+    static isDebug = false;
+
+    // 是否清除没有在模型定义的数据
+    static isRemoveAddition = true;
+
+    // 模型字段
+    static fields = {
+      title: 'string',
+      thumb: 'string',
+      thumbUri: 'string',
+      displayUrl: 'string',
+      scaleRatio: 'object',
+      originWidth: 'number',
+      originHeight: 'number',
+      isShowDialog: 'object',
+      isFromSDK: 'boolean',
+    };
+
+    // 默认数据
+    static defaultData = {
+      ctrlRatioLocked: true,
+      title: '',
+      thumb: '',
+      thumbUri: '',
+      displayUrl: '',
+      scaleRatio: {
+        width: 1,
+        height: 1,
+      },
+      originHeight: 0,
+      originWidth: 0,
+      isShowDialog: { flag: false },
+      isFromSDK: false,
+    };
+
+    // TODO 必须!!!
+    // static toJsonx(modelData) {
+    //   const jsonx = new TeachingDrawingBoardCardSaveInfo(modelData).toJsonx();
+    //   return jsonx;
+    // }
+
+    resize(width, height, scale) {
+      const scaleRatio = {
+        width: width / this.get('originWidth'),
+        height: height / this.get('originHeight'),
+      };
+      this.merge({
+        scaleRatio,
+        width,
+        height,
+      });
+    }
+
+    // 转换数据以适应模型
+    static transform(modelData) {
+      return modelData.merge({
+        originWidth: modelData.get('width'),
+        originHeight: modelData.get('height'),
+      });
+    }
+
+    // 在模型实例化前会调用该方法检查是否不需要渲染，只有返回true才会不渲染
+    static notRender(props, context) {
+      return false;
+    }
+
+    // 初始化模型
+    init(props) {
+      // console.log(props);
+    }
+
+    /** enow-SDK方法 start */
+
+    /**
+     * 显示第三方资源
+     */
+    openDialog() {
+      this.merge({ isShowDialog: { flag: true }, isFromSDK: true });
+    }
+
+    /**
+     * 关闭第三方资源
+     */
+    closeDialog() {
+      this.merge({ isShowDialog: { flag: false }, isFromSDK: true });
+    }
+
+    /** enow-SDK方法 end */
+  }
+
+}

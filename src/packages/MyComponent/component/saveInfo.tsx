0dbfe0ecda0c93a2af907dc148604f11ac8dbd7f
刘少杰 <liushaojie@cvte.com>
2021-06-10 21:47:12 +0800

    feat: drop useless

--- src/packages/MyComponent/component/saveInfo.tsx
+++ src/packages/MyComponent/component/saveInfo.tsx
@@ -3,31 +3,18 @@ import { name, nameCamel } from "../const";
 export default function (dependencies) {
   const { ElementSaveInfo } = dependencies.saveInfo;
   return class RemoteComponentSaveInfo extends ElementSaveInfo {
-    constructor(data) {
-      super(data);
-      this.topmodel = {};
-      if (!data) {
-        return
-      }
-      const {
-        x, y, width, height
-      } = data;
-      Object.assign(this.topmodel, {
-        x, y, width, height
-      })
-    }
 
-    // toJsonx() {
-    //   const { x, y, width, height } = this.topmodel;
-    //   return {
-    //     remoteComponent: {
-    //       x,
-    //       y,
-    //       width,
-    //       height,
-    //     }
-    //   }
-    // }
+    toJsonx() {
+      const { x, y, width, height } = this.topmodel;
+      return {
+        [nameCamel]: {
+          x,
+          y,
+          width,
+          height,
+        }
+      }
+    }
 
     toModelJson(jsonx) {
       const jsonxContent = jsonx[nameCamel];

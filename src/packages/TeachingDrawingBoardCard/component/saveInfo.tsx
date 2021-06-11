f7657c629be4a9076230d402385580664f205729
刘少杰 <liushaojie@cvte.com>
2021-06-11 11:57:02 +0800

    feat: teaching的saveinfo

--- src/packages/TeachingDrawingBoardCard/component/saveInfo.tsx
+++ src/packages/TeachingDrawingBoardCard/component/saveInfo.tsx
@@ -2,35 +2,21 @@ import { name, nameCamel } from "../const";
 
 export default function (dependencies) {
   const { ElementSaveInfo } = dependencies.saveInfo;
-  return class PictureSaveInfo extends ElementSaveInfo {
-    constructor(data) {
-      super(data);
-      this.topmodel = {};
-      if (!data) {
-        return
-      }
-      const {
-        x, y, width, height
-      } = data;
-      Object.assign(this.topmodel, {
-        x, y, width, height
-      })
-    }
+  return class SaveInfo extends ElementSaveInfo {
+    static isRemote = true;
 
-    toJsonx() {
-      const { x, y, width, height } = this.topmodel;
+    dataToJsonx() {
+      console.error('topmodel teaching', this.topmodel, this.data)
       return {
-        remoteComponent: {
-          x,
-          y,
-          width,
-          height,
+        [nameCamel]: {
+          ...(this.toJsonx().common),
+          title: this.data.title,
+          thumbUri: this.data.thumbUri,
         }
       }
     }
 
     toModelJson(jsonx) {
-      console.error('toModelJsontoModelJsontoModelJson', jsonx, nameCamel)
       const jsonxContent = jsonx[nameCamel];
       const superJson = super.toModelJson(jsonxContent);
 
